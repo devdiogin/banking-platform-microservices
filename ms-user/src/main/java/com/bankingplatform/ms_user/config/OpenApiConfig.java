1a6d41c2bf2672bb95d549f0ef4fcf84af6c8219
diogenes.cassimiro <dev.diogelucas@gmail.com>
2026-07-24 13:24:52 -0300

    fix: ajuste no swagger

--- ms-user/src/main/java/com/bankingplatform/ms_user/config/OpenApiConfig.java
+++ ms-user/src/main/java/com/bankingplatform/ms_user/config/OpenApiConfig.java
@@ -3,19 +3,23 @@ package com.bankingplatform.ms_user.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
-@Component
+@Configuration
 public class OpenApiConfig {
 
+    @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI().info(
-                new Info().title("ms-User")
-                        .description("DocumentaÁ„o do microsservice de usu·rio")
+        return new OpenAPI()
+                .info(new Info()
+                        .title("MS User API")
+                        .version("1.0.0")
+                        .description("API respons√°vel pelo gerenciamento de usu√°rios da Banking Platform.")
                         .contact(new Contact()
-                                .name("dev-diogin")
+                                .name("Diogenes Cassimiro")
                                 .email("dev.diogelucas@gmail.com")
-                                .url("https://www.linkedin.com/in/dev-cassimiro/"))
-        );
+                                .url("https://www.linkedin.com/in/dev-cassimiro/")));
     }
 }
