0ff98e78bc85dd020f40ca94ca0b91dab62a9302
Dev-Diogelucasc <diogenes.cassimiro.dev@gmail.com>
2026-08-25 21:07:50 -0300

    feat: testes unitarios com junit e mockito

--- service/ms-customer/src/test/java/com/banking/ms_customer/MsCustomerApplicationTests.java
+++ service/ms-customer/src/test/java/com/banking/ms_customer/MsCustomerApplicationTests.java
@@ -1,13 +1,14 @@
 package com.banking.ms_customer;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
 class MsCustomerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(MsCustomerApplication.class).isNotNull();
 	}
 
 }
