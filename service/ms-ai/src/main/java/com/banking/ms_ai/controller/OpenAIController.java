e02b3db781df03fc150d13dcf391040f23274aa7
Diogenes Cassimiro <diogenes.cassimiro.dev@gmail.com>
2026-08-27 16:35:04 -0300

    fix: ms-ia funcionando

--- service/ms-ai/src/main/java/com/banking/ms_ai/controller/OpenAIController.java
+++ service/ms-ai/src/main/java/com/banking/ms_ai/controller/OpenAIController.java
@@ -2,6 +2,8 @@ package com.banking.ms_ai.controller;
 
 import com.banking.ms_ai.dto.OpenAIRequestDto;
 import com.banking.ms_ai.service.OpenAIService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/ai")
 @RequiredArgsConstructor
+@Tag(name = "Controller AI")
 public class OpenAIController {
 
     private final OpenAIService openAIService;
 
     @PostMapping("/chat")
+    @Operation(summary = "Realizar a Busca com Chat")
     public ResponseEntity<String> chat(@RequestBody OpenAIRequestDto dto) {
         return ResponseEntity.ok(openAIService.chat(dto.message()));
     }
