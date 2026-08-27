30dca9007f1c1fc9f3e70f8e7f8c8c7fe876258f
Diogenes Cassimiro <diogenes.cassimiro.dev@gmail.com>
2026-08-27 16:11:25 -0300

    feat: pastas que nao foram commitada

---
+++ service/ms-ai/src/main/java/com/banking/ms_ai/MsAiApplication.java
@@ -0,0 +1,13 @@
+package com.banking.ms_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAiApplication.class, args);
+	}
+
+}
---
+++ service/ms-ai/src/test/java/com/banking/ms_ai/MsAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.ms_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
